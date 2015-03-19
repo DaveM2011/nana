1505c8d729f8d68cb9015c3f0b430a47f09ac0a0
DaveM2011 <davem.2011@gmail.com>
2015-03-19 01:09:46 +0000

    Added repeatable property to repeat picture
    picture pic(fm);
    pic.load(STR("test.png"));
    pic.repeatable(true);
    
    I suppose this could easily extend it but it works and is the most
    simple approach to repeating an image

--- include/nana/gui/widgets/picture.hpp
+++ include/nana/gui/widgets/picture.hpp
@@ -57,7 +57,7 @@ namespace nana
 
 		/// Enables the image to be stretched to the widget size.
 		void stretchable(unsigned left, unsigned top, unsigned right, unsigned bottom);
-		
+
 		/// Enables/disable the image to be repeated without changing aspect ratio.
 		void repeatable(bool);
 		
